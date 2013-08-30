00ef90c69d6514c38a6e6f05e190637ab586c0da
Sihasiel <Be_officiel@live.fr>
2013-08-30 17:19:07 +0200

    Create Hack the planet.py

---
+++ Hack the planet.py
@@ -0,0 +1,8 @@
+===========
+Hack The Planet 
+===========
+
+site = raw_input('Site à hacker ? ')
+print(site,"a été hacked lol")
+print('Suppression de tout les fichiers .. Dl .. Merci pour tes données bancaires ')
+raw_input(':( ')
