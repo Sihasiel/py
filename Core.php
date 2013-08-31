1d2fe59c3457300d659bcef9eaa45505937b8ec4
Sihasiel <Antasyde@live.fr>
2013-08-31 10:15:34 +0200

    Core.php
    Codé avec les pieds

---
+++ Core.php
@@ -0,0 +1,21 @@
+<?php
+#|#|#|#|#|#|#|#|#|#|#|#|#|#|
+#|                        #|
+#|          S             #|
+#|                        #|
+#|#|#|#|#|#|#|#|#|#|#|#|#|#|
+if isset($_GET['username'])
+{
+}
+else
+{
+  echo 'no session found')
+}
+$useragent = array ('ie');
+  if ($_server['HTTP_USER_AGENT'] == $useragent)
+  {
+    echo "$_GET['username'] , le navigateur ie à été rejeté, ";
+    
+  }
+ 
+?>
